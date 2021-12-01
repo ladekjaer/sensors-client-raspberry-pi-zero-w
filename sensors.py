ae7b6d5f57c51d4f87bf04e953ca4a415617b2a6
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2021-12-01 15:26:34 +0100

    Write local db with measurements.

--- sensors.py
+++ sensors.py
@@ -6,12 +6,15 @@ import socket
 import time
 import uuid
 from w1thermsensor import W1ThermSensor
+import sqlite3
 
+# Get settings from environment variables
 url = os.getenv('URL')
 sensors_api_key = os.getenv('SENSORS_API_KEY')
 interval = os.getenv('INTERVAL')
 hostname = socket.gethostname()
 
+# Quit if settings in missing or ill formed
 if not url or not sensors_api_key or not interval:
 	print('You must supply environment variable URL, INTERVAL and SENSORS_API_KEY')
 	sys.exit(1)
@@ -25,6 +28,17 @@ else:
 
 hdr = {'ACCESS-KEY': sensors_api_key}
 
+# Create database, if it does not already exists
+con = sqlite3.connect('sensors.db')
+cur = con.cursor()
+cur.execute('''	CREATE TABLE IF NOT EXISTS measurements (
+					timestamp text NOT NULL,
+					thermostat_id text NOT NULL,
+					hostname text NOT NULL,
+					temp real NOT NULL,
+					pi_id text NOT NULL,
+					uuid text NOT NULL) ''')
+
 def getserial():
 	# Extract serial from cpuinfo file
 	cpuserial = "0000000000000000"
@@ -36,7 +50,7 @@ def getserial():
 		f.close()
 	except:
 		cpuserial = "ERROR000000000"
- 
+
 	return cpuserial
 
 pi_id = getserial()
@@ -58,8 +72,16 @@ def getsensordata():
 
 	return sensordata
 
+def write_measurements_to_database(measurements):
+	for measurement in measurements:
+		cur.execute("INSERT INTO measurements VALUES (:timestamp, :thermostat_id, :hostname, :temp, :pi_id, :uuid)", measurement)
+	con.commit()
+
 while True:
-	sensordata = getsensordata()
+	sensordata = getsensordata() #Read sensor data from the DS18B20 temperatures
+	write_measurements_to_database(sensordata) # insert sensordata in DB
+	# read from DB
+		# post read data in batches
 	res = requests.post(url, json = sensordata, headers = hdr)
 	print("%s %s" % (res.status_code, res.text))
 	time.sleep(interval)
